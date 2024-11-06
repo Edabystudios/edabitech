1fe2300403bcc5615caaffb4c52b0ac982aed1ce
Edabz <125823275+Edabystudios@users.noreply.github.com>
2024-11-06 13:08:10 +0100

    Update and rename script.js to search.js

---
+++ script/search.js
@@ -0,0 +1,12 @@
+ function performSearch() {
+        const query = document.getElementById("searchInput").value;
+        
+        if (query) {
+            // Here, you can add code to perform an actual search, such as redirecting to a search page or calling an API.
+            alert("You searched for: " + query);
+        } else {
+            alert("Please enter a search query.");
+        }
+    }
+ 
+      
